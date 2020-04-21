88549c8e28d7a9bc1329271cf631e8a5312261fb
Shinya Uryu <suika1127@gmail.com>
2020-04-21 12:16:42 +0900

    Fix docomo new format

--- R/collect_corona_jp.R
+++ R/collect_corona_jp.R
@@ -10,12 +10,15 @@ collect_corona_go_jp <- function(source) {
   . <- data <- value <- type <- text <- NULL
   rlang::arg_match(source,
                    c("agoop", "docomo"))
+  source <-
+    dplyr::recode(source,
+                  `docomo` = "docomo_zenkoku") # docomo_todohuken
   ts <-
     round(as.numeric(lubridate::now()), digits = 0)
   df_source <-
-    paste0("https://corona.go.jp/assets/js/",
+    paste0("https://corona.go.jp/toppage/json/",
            source,
-           "_data.js?_=",
+           "_data.json?_=",
            ts) %>%
     jsonlite::fromJSON()
   d1 <-
@@ -28,7 +31,9 @@ collect_corona_go_jp <- function(source) {
     purrr::pluck("data") %>%
     tidyr::unnest(cols = data) %>%
     dplyr::mutate_if(is.factor, as.character) %>%
-    dplyr::mutate(value = as.numeric(value)) %>%
+    dplyr::mutate(area = stringr::str_replace_all(area, "<br>", "_"),
+                  value = as.numeric(value),
+                  text = stringr::str_remove_all(text, "\u203b")) %>%
     dplyr::mutate(value = dplyr::if_else(type == "low", -value, value)) %>%
     purrr::discard(names(.) == "type") %>%
     tidyr::pivot_wider(
